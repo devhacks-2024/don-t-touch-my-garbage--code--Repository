3726e0e689c09c9e8211c0718108c39d187dd3c0
Fidelio Ciandy <fiocheah08@gmail.com>
2024-02-23 17:19:20 -0600

    test

--- tinder/src/App.js
+++ tinder/src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import loginPage from "./pages/loginpage";
 
 function App() {
   return (
@@ -15,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <loginPage />
         </a>
       </header>
     </div>
