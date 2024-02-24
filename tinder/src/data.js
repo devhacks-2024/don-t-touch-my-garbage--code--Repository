ef96728ae13c8f278c5fff1195bbcbf41be19fba
18nghiado <doducnghia2002@gmail.com>
2024-02-23 20:30:17 -0600

    add image

--- tinder/src/data.js
+++ tinder/src/data.js
@@ -3,10 +3,10 @@ class PeopleObj {
         if(arguments.length ===0 )
         {
             this.id = 'guest';
-            this.image ='path/to/default/image.jpg'; // Default image path
-            this.age = 30; // Age
-            this.gender = 'Non-binary'; // Gender
-            this.bio = 'Just exploring the world of software engineering.'; // Bio
+            this.image ='path/to/default/image.jpg'; 
+            this.age =30;
+            this.gender = 'Non-binary'; 
+            this.bio = 'Just exploring the world of software engineering.'; 
             this.password = "guest";
         }
         else
@@ -46,16 +46,22 @@ class PeopleObj {
     }
 }
 
-const users = [];
-const user1 = new PeopleObj(1, 'path/to/image.jpg', 25, 'Male', 'Software engineer with a passion for coding challenges.', "password");
-const user2 = new PeopleObj(2, 'path/to/image.jpg', 26, 'Male', 'Software engineer with a passion for coding challenges.', "password");
-const user3 = new PeopleObj(3, 'path/to/image.jpg', 27, 'Male', 'Software engineer with a passion for coding challenges.', "password");
-const user4 = new PeopleObj(4, 'path/to/image.jpg', 28, 'Male', 'Software engineer with a passion for coding challenges.', "password");
-users.push(user1);
-users.push(user2);
-users.push(user3);
-users.push(user4);
-users.push(new PeopleObj());
+const users = [
+    new PeopleObj(1, 'path/to/image2.jpg', 29, 'Female', 'Enthusiastic about mobile development and UX design.',"password"),
+    new PeopleObj(2, 'path/to/image2.jpg', 28, 'Female', 'Enthusiastic about mobile development and UX design.',"password"),
+    new PeopleObj(3, 'path/to/image3.jpg', 32, 'Male', 'Full-stack developer with a love for JavaScript and Python.',"password"),
+    new PeopleObj(4, 'path/to/image4.jpg', 29, 'Non-binary', 'AI enthusiast and data scientist.',"password"),
+    new PeopleObj(5, 'path/to/image5.jpg', 35, 'Female', 'DevOps engineer focusing on cloud infrastructure and security.',"password"),
+    new PeopleObj(6, 'path/to/image.jpg', 30, 'Female', 'Software engineer with a passion for coding challenges.', "password"),
+    new PeopleObj(7, 'path/to/image.jpg', 31, 'Male', 'Software engineer with a passion for coding challenges.', "password"),
+    new PeopleObj(8, 'path/to/image.jpg', 32, 'Male', 'Software engineer with a passion for coding challenges.', "password"),
+    new PeopleObj(9, 'path/to/image.jpg', 33, 'Non-Binary', 'Software engineer with a passion for coding challenges.', "password"),
+    new PeopleObj(10, 'path/to/image.jpg', 34, 'Male', 'Software engineer with a passion for coding challenges.', "password"),
+    new PeopleObj(11, 'path/to/image.jpg', 35, 'Male', 'Software engineer with a passion for coding challenges.', "password"),
+    new PeopleObj(12, 'path/to/image.jpg', 36, 'Female', 'Software engineer with a passion for coding challenges.', "password")
+];
+
+
 console.log(users[4]);
 
 class QuestionsObj {
@@ -77,28 +83,79 @@ class QuestionsObj {
     }
 }
 
-const questions = [];
-const easyQuestion = new QuestionsObj(
-    1,
-    'What is the output of `console.log(1 + "2" + "2");` in JavaScript?',
-    'easy',
-    "122",
-    ['"122"', '"32"', '"14"']
-);
-
-// Hard question (fill in the blank)
-const hardQuestion = new QuestionsObj(
-    2,
-    'Fill in the blank: The _______ method is used to add one or more elements to the end of an array.',
-    'hard',
-    'push'
-);
+const questions = [
+    new QuestionsObj(
+        1,
+        'What is the output of `console.log(1 + "2" + "2");` in JavaScript?',
+        'easy',
+        0,
+        ['"122"', '"32"', '"14"']
+    ),
+    
+    new QuestionsObj(
+        2,
+        'What is the output of `console.log(1 + "2" + "2");` in JavaScript?',
+        'easy',
+        0,
+        ['"122"', '"32"', '"14"']
+    ),
+    new QuestionsObj(
+        3,
+        'Which of the following is a JavaScript framework?',
+        'easy',
+        1,
+        ['Python', 'React', 'Django', 'Flask']
+    ),
+    new QuestionsObj(
+        4,
+        'What does HTML stand for?',
+        'easy',
+        2,
+        ['Hyper Trainer Marking Language', 'Hyper Text Marketing Language', 'Hyper Text Markup Language', 'Hyper Text Markup Leveler']
+    ),
+    new QuestionsObj(
+        5,
+        'Fill in the blank: ______ is the process of finding and fixing errors in software.',
+        'hard',
+        'Debugging'
+    ),
+    new QuestionsObj(
+        6,
+        'What is the command to install a package using npm?',
+        'hard',
+        'npm install <package_name>'
+    ),
+    new QuestionsObj(
+        7,
+        'Fill in the blank: The _______ method is used to add one or more elements to the end of an array.',
+        'hard',
+        'push'
+    ),
+    
+    new QuestionsObj(
+        8,
+        'Fill in the blank: The _______ method is used to add one or more elements to the end of an array.',
+        'hard',
+        'push'
+    ),
+    
+    QuestionsObj(
+        9,
+        'Fill in the blank: The _______ method is used to add one or more elements to the end of an array.',
+        'hard',
+        'push'
+    ),
+    
+    QuestionsObj(
+        10,
+        'Fill in the blank: The _______ method is used to add one or more elements to the end of an array.',
+        'hard',
+        'push'
+    )
+];
 
 // Checking answer
-questions.push(easyQuestion);
-questions.push(hardQuestion);
-
 console.log(questions[0].checkAnswer('"122"')); 
-console.log(questions[1].checkAnswer('push')); 
+console.log(questions[7].checkAnswer('push')); 
 
 export default {users, questions};
