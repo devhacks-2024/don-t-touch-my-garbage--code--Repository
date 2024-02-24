bd0eab0701a8fd1aa46f33ada4cbfa15d1211f1f
Fidelio Ciandy <fiocheah08@gmail.com>
2024-02-23 18:36:24 -0600

    make login page without decoration

--- tinder/src/App.js
+++ tinder/src/App.js
@@ -3,24 +3,7 @@ import "./App.css";
 import LoginPage from "./front_pages/LoginPage.js";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <LoginPage />
-        </a>
-      </header>
-    </div>
-  );
+  return <LoginPage />;
 }
 
 export default App;
--- tinder/src/front_pages/LoginPage.js
+++ tinder/src/front_pages/LoginPage.js
@@ -1,5 +1,56 @@
+import { Box, Button } from "@mui/material";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+
 function LoginPage() {
-  return <div> Hello woopjwejpwqe </div>;
+  return (
+    <div>
+      <ToolBar />
+      <Inputs />
+    </div>
+  );
+}
+
+function ToolBar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Code Crasher
+        </Typography>
+      </Toolbar>
+    </Box>
+  );
+}
+
+function Inputs() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      username: data.get("username"),
+      password: data.get("password"),
+    });
+  };
+  return (
+    <Box component="form" onSubmit={handleSubmit}>
+      <div>
+        <div>
+          <TextField name="username" label="Username" variant="outlined" />
+          <TextField
+            name="password"
+            label="Password"
+            type="password"
+            variant="outlined"
+          />
+        </div>
+        <Button type="submit" id="login-button">
+          Login
+        </Button>
+      </div>
+    </Box>
+  );
 }
 
 export default LoginPage;
